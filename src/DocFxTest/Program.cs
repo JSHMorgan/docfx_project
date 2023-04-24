dd7890e8eb7aac76c33f69b7693e33d3a034ab71
Xreality <JSHMorgan@outlook.com>
2023-04-24 16:32:27 +0100

    Initial commit

---
+++ src/DocFxTest/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DocFxTest;
+
+/// <summary>
+/// Docs
+/// </summary>
+/// <param name="args"></param>
+internal class Program
+{
+    /// <summary>
+    /// Docs
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
