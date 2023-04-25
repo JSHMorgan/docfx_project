de43312c367e78cf3eb2e914545addbbf3100499
Xreality <JSHMorgan@outlook.com>
2023-04-25 14:30:44 +0100

    asd

---
+++ src/DocFxTest/MyMaths.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace DocFxTest;
+
+/// <summary>
+/// My Maths class.
+/// </summary>
+internal class MyMaths
+{
+    public static int TimesUsed { get; private set;}
+
+    public static T Add<T>(T left, T right) where T : INumber<T>
+    {
+        SetTimesUsed();
+        return left + right;
+    }
+
+    /// <summary>
+    /// Private function to iterate TimesUsed property.
+    /// </summary>
+    private static void SetTimesUsed()
+    {
+        TimesUsed++;
+    }
+}
--- src/DocFxTest/Program.cs
+++ src/DocFxTest/Program.cs
@@ -3,7 +3,6 @@
 /// <summary>
 /// Docs
 /// </summary>
-/// <param name="args"></param>
 internal class Program
 {
     /// <summary>
@@ -12,6 +11,17 @@ internal class Program
     /// <param name="args"></param>
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(HelloWorld());
+        int sum = MyMaths.Add(4, 4);
+        Console.WriteLine(sum);
+    }
+
+    /// <summary>
+    /// A function to give the user "Hello, World!" in a string format.
+    /// </summary>
+    /// <returns>A string of the text "Hello, World!"</returns>
+    public static string HelloWorld()
+    {
+        return "Hello, World!";
     }
 }
